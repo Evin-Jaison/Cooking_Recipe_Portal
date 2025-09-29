5ff2d7dfdb7b73ddf0b43aff5d1000e4acebc0c0
Evin Jaison <CHN23CSC307@ceconline.edu>
2025-09-29 12:47:47 +0530

    Remove MySQL password from db.js
    Removed sensitive password from db.js for security.

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Evin@2005isme", // your MySQL password
+  password: "", // your MySQL password
   database: "recipe_portal"
 });
 
